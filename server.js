126a72a8367c37f198e2968a480409df94aed207
Abhinav Sharma <sharma.abhinav503@gmail.com>
2019-11-14 23:53:45 -0500

    Clearing directory structure for deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.use('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'public','index.html'));
+})
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log('Server listening on: ', port);
+})
